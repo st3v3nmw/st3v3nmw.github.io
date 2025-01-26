421608eceb839e02d3738422d9b0659068e30071
st3v3nmw <st3v3nmw@users.noreply.github.com>
2025-01-26 12:13:11 +0000

    deploy: fc789d913fed290e5a80e6c2df46303907e44eb4

--- js/theme.js
+++ js/theme.js
@@ -1,22 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const codeBlocks = document.querySelectorAll('pre > code');
+if (window.innerWidth > 768) {
+    document.addEventListener('DOMContentLoaded', () => {
+        const codeBlocks = document.querySelectorAll('pre > code');
 
-    codeBlocks.forEach(codeBlock => {
-        const copyButton = document.createElement('button');
-        copyButton.innerHTML = '<i class="fa-solid fa-copy"></i> Copy';
-        copyButton.classList.add('copy-btn');
+        codeBlocks.forEach(codeBlock => {
+            const copyButton = document.createElement('button');
+            copyButton.innerHTML = '<i class="fa-solid fa-copy"></i> Copy';
+            copyButton.classList.add('copy-btn');
 
-        copyButton.addEventListener('click', () => {
-            const code = codeBlock.textContent;
-            navigator.clipboard.writeText(code).then(() => {
-                copyButton.innerHTML = '<i class="fa-solid fa-check"></i> Copied!';
-                setTimeout(() => {
-                    copyButton.innerHTML = '<i class="fa-solid fa-copy"></i> Copy';
-                }, 2000);
+            copyButton.addEventListener('click', () => {
+                const code = codeBlock.textContent;
+                navigator.clipboard.writeText(code).then(() => {
+                    copyButton.innerHTML = '<i class="fa-solid fa-check"></i> Copied!';
+                    setTimeout(() => {
+                        copyButton.innerHTML = '<i class="fa-solid fa-copy"></i> Copy';
+                    }, 2000);
+                });
             });
-        });
 
-        codeBlock.parentNode.style.position = 'relative';
-        codeBlock.parentNode.appendChild(copyButton);
+            codeBlock.parentNode.style.position = 'relative';
+            codeBlock.parentNode.appendChild(copyButton);
+        });
     });
-});
+}
